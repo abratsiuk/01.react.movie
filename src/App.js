6b9de4c40a18db8c6c7676683345bc9e14e6e3ec
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-10 10:45:59 +0300

    s40

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 // don't change the Component name "App"
 export default class App extends React.Component {
+
+  constructor(){
+    this.state = {movies:[]}
+  }
+
+  componentDidMount(){
+    console.log('q')
+  }
+  
   render() {
     // TODO: implement component
     return <main className="container content"></main>;
