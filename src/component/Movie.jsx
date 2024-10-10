932baaf6d959ebd60593c2da204e46df331cf2df
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-10 15:31:40 +0300

    s40

--- src/component/Movie.jsx
+++ src/component/Movie.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 
 function Movie(props) {
-    const movie = props.movie;
+    const {
+        Title: title,
+        Year: year,
+        imdbID: id,
+        Type: type,
+        Poster: poster,
+    } = props;
+
     return (
-            <div className="movie card">
-                <div className="card-image">
-                <img src={movie.Poster} alt=''/>
-                <span className="card-title">{movie.Title}</span>
-                </div>
-                <div className="card-content">
-                <p>{movie.Type}, {movie.Year}</p>
-                </div>
-            </div>
+        <div id={id} className='card movie'>
+        <div className='card-image waves-effect waves-block waves-light'>
+            {poster === 'N/A' ? (
+                <img
+                    className='activator'
+                    src={`https://via.placeholder.com/300x400?text=${title}`}
+                />
+            ) : (
+                <img className='activator' src={poster} />
+            )}
+        </div>
+        <div className='card-content'>
+            <span className='card-title activator grey-text text-darken-4'>
+                {title}
+            </span>
+            <p>
+                {year} <span className='right'>{type}</span>
+            </p>
+        </div>
+    </div>
     );
 }
 
--- src/component/Movies.jsx
+++ src/component/Movies.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Movie } from './Movie';
 
 function Movies(props) {
-    const movies = props.movies;
-    console.log(movies);
-    return (<div className="movies">
-        {movies.map(movie=>(
-            <Movie key={movie.imdbID} movie={movie}/>
-        ))}
-    </div>);
+    const {movies = []} = props;
+
+    return (
+        <div className="movies">
+            {movies.map(movie=>(
+                <Movie key={movie.imdbID} {...movie}/>
+            ))}
+        </div>
+    );
 }
 
 export { Movies };
