8d79ca4ae9989ecdce9d6a1135968280ba4d8231
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-10 14:59:56 +0300

    s40

--- src/Movie.jsx
+++ src/Movie.jsx
@@ -3,19 +3,15 @@ import React from 'react';
 function Movie(props) {
     const movie = props.movie;
     return (
-        <div className="row">
-            <div className="col s12 m7">
-            <div className="card">
+            <div className="movie card">
                 <div className="card-image">
                 <img src={movie.Poster} alt=''/>
                 <span className="card-title">{movie.Title}</span>
                 </div>
                 <div className="card-content">
-                <p>{movie.Type},{movie.Year}</p>
+                <p>{movie.Type}, {movie.Year}</p>
                 </div>
             </div>
-            </div>
-        </div>
     );
 }
 
