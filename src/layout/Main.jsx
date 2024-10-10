297de9282892e9901e8a39151cbb9fd80b185bb9
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-10 16:08:44 +0300

    add preloader

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Movies } from '../component/Movies';
+import { Preloader } from '../component/Preloader';
 
 class Main extends React.Component  {
-  state = {movies:[]};
+  state = {
+    movies:[],
+    loading:true
+  };
 
   componentDidMount(){
+    this.setState({loading:true});
     fetch('http://www.omdbapi.com/?apikey=8680c009&s=matrix')
       .then(response=>response.json())
-      .then(data=>this.setState({movies:data.Search}))
+      .then(data=>this.setState({movies:data.Search, loading:false}))
       .catch(console.error);      
   }
   
   render() {
-    const {movies} = this.state;
+    const {movies, loading} = this.state;
     return (<main className="container content">
-      <Movies movies={movies}/>
+      {loading ? 
+          <Preloader/> :
+          <Movies movies={movies}/>}
     </main>);
   }
 }
