41ee64d8f16e34a86aa1aae09fcdac6028465d0b
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-10-18 16:49:50 +0300

    refactor Search component to use functional hooks and simplify state management

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,86 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class Search extends React.Component {
-    state = {
-        search: '',
-        type: 'all',
+function Search(props) {
+    const [search, setSearch] = useState('');
+    const [type, setType] = useState('all');
+    const { reloadMovies } = props;
+
+    const handleType = (event) => {
+        setType(event.target.dataset.type);
+        reloadMovies(search, event.target.dataset.type);
     };
 
-    handleType = (event) => {
-        this.setState({ type: event.target.dataset.type }, this.handleFilter);
-    };
-
-    handleFilter = () => {
-        this.props.reloadMovies(this.state.search, this.state.type);
-    };
-
-    render() {
-        const { search } = this.state;
-
-        return (
-            <div className='row'>
-                <div className='input-field'>
-                    <input
-                        placeholder='search'
-                        type='search'
-                        className='validate'
-                        value={search}
-                        onChange={(event) =>
-                            this.setState({ search: event.target.value })
+    return (
+        <div className='row'>
+            <div className='input-field'>
+                <input
+                    placeholder='search'
+                    type='search'
+                    className='validate'
+                    value={search}
+                    onChange={(event) => setSearch(event.target.value)}
+                    onKeyDown={(event) => {
+                        if (event.code === 'Enter') {
+                            reloadMovies(search, type);
                         }
-                        onKeyDown={(event) => {
-                            if (event.code === 'Enter') {
-                                this.handleFilter();
-                            }
-                        }}
-                    />
-                    <button
-                        className='btn search-btn light-blue accent-4'
-                        onClick={this.handleFilter}
-                    >
-                        Search
-                    </button>
+                    }}
+                />
+                <button
+                    className='btn search-btn light-blue accent-4'
+                    onClick={() => reloadMovies(search, type)}
+                >
+                    Search
+                </button>
 
-                    <div>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='type'
-                                type='radio'
-                                data-type='all'
-                                onChange={this.handleType}
-                                checked={this.state.type === 'all'}
-                            />
-                            <span>All</span>
-                        </label>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='type'
-                                type='radio'
-                                data-type='movie'
-                                onChange={this.handleType}
-                                checked={this.state.type === 'movie'}
-                            />
-                            <span>Movies only</span>
-                        </label>
-                        <label>
-                            <input
-                                className='with-gap'
-                                name='type'
-                                type='radio'
-                                data-type='series'
-                                onChange={this.handleType}
-                                checked={this.state.type === 'series'}
-                            />
-                            <span>Series only</span>
-                        </label>
-                    </div>
+                <div>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='all'
+                            onChange={handleType}
+                            checked={type === 'all'}
+                        />
+                        <span>All</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='movie'
+                            onChange={handleType}
+                            checked={type === 'movie'}
+                        />
+                        <span>Movies only</span>
+                    </label>
+                    <label>
+                        <input
+                            className='with-gap'
+                            name='type'
+                            type='radio'
+                            data-type='series'
+                            onChange={handleType}
+                            checked={type === 'series'}
+                        />
+                        <span>Series only</span>
+                    </label>
                 </div>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 Search.propTypes = {
